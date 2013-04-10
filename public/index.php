c360ecf5d85ac84f985c858932c3d1f5e2b4bcd4
Vladimir Goldobin <vgold@xiag.ch>
2013-04-10 16:25:07 +0700

    barberry instance for Bina Flyer printing tool

--- public/index.php
+++ public/index.php
@@ -3,5 +3,7 @@ namespace Barberry;
 
 include dirname(__DIR__) . '/vendor/autoload.php';
 
-$application = new Application(new Config(realpath(__DIR__ . '/../'), '/etc/config.php'));
+$application = new Application(new Config(realpath(
+    __DIR__ . '/../'
+), '/etc/config.php'), new \Barberry\Filter\OpenOfficeTemplate('/tmp', new \clsTinyButStrong()));
 $application->run()->send();
